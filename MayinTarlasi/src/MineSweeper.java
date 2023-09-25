e2ee22b69f04daaa1c62fe2d4397c4e5d260760d
fturannn <furkn.turan@hotmail.com>
2023-09-25 15:23:45 +0300

    Added check function

--- MayinTarlasi/src/MineSweeper.java
+++ MayinTarlasi/src/MineSweeper.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MineSweeper {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int row, column, guessRow, guessColumn;
+        int row, column, guessedRow, guessedColumn;
 
         System.out.println("Welcome to Mine Sweeper Game!");
         System.out.println("Please specify game board size");
@@ -17,42 +17,45 @@ public class MineSweeper {
 
         System.out.println("Game board size is specified " + row + "x" + column);
 
+        // Create Game and Mined Boards' Matrices
         char [][] gameBoard = new char[row][column];
+        char [][] placedBoard = new char[row][column];
 
         System.out.println("Location of the Mines");
-        placer(row, column, gameBoard); // This is a method which is place mines to the Game Board
-        showMines(row, column, gameBoard);
-        
+        placer(row, column, placedBoard); // Places mines to the Game Board and shows them
+        boardMaker(row, column, gameBoard); // Builds hidden Game Board
+        while (true){
+            System.out.print("Satır giriniz: ");
+            guessedRow = input.nextInt();
+
+            System.out.print("Sütun giriniz: ");
+            guessedColumn = input.nextInt();
+
+            writePoint(row, column, guessedRow, guessedColumn, placedBoard, gameBoard);
+        }
     }
-
+    
     static void placer(int row, int column, char [][] gameBoard) {
-        int numOfMines = row * column / 4;
+        int mines = row * column / 4;
 
-        for (int i = 0; i < numOfMines; i++) {
+        for (int i = 0; i < mines; i++) {
             int mineRow = (int) (Math.random() * row);
             int mineColumn = (int) (Math.random() * column);
             if (gameBoard[mineRow][mineColumn] == '*') {
-                numOfMines++;
+                mines++;
             } else {
                 gameBoard[mineRow][mineColumn] = '*';
             }
         }
-    }
 
-    static void showMines(int row, int column, char [][] gameBoard) {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 if (gameBoard[i][j] == '*') {
-                    continue;
+                    System.out.print(gameBoard[i][j] + " ");
                 } else {
                     gameBoard[i][j] = '-';
+                    System.out.print(gameBoard[i][j] + " ");
                 }
-            }
-        }
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                System.out.print(gameBoard[i][j] + " ");
             }
             System.out.println();
         }
@@ -64,6 +67,44 @@ public class MineSweeper {
             for (int j = 0; j < column; j++) {
                 gameBoard[i][j] = '-';
             }
+        }
+    }
+
+    static void writePoint(int row, int column, int guessedRow, int guessedColumn, char [][] placedBoard, char [][] gameBoard) {
+        int r = guessedRow - 1;
+        int c = guessedColumn - 1;
+        int count = 0;
+        if (placedBoard[r][c] == '-') {
+            if (placedBoard[r-1][c-1] == '*') {
+                count++;
+            }
+            if (placedBoard[r-1][c] == '*') {
+                count++;
+            }
+            if (c+1 < column - 1 && placedBoard[r-1][c+1] == '*') {
+                count++;
+            }
+            if (placedBoard[r][c-1] == '*') {
+                count++;
+            }
+            if (c+1 < column - 1 && placedBoard[r][c+1] == '*') {
+                count++;
+            }
+            if (r+1 < row - 1 && placedBoard[r+1][c-1] == '*') {
+                count++;
+            }
+            if (r+1 < row - 1 && placedBoard[r+1][c] == '*') {
+                count++;
+            }
+            if (r+1 < row - 1 && c+1 < column - 1 && placedBoard[r+1][c+1] == '*') {
+                count++;
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(gameBoard[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
