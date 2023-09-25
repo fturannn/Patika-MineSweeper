11952149373f2e82b86957ea744b9d20d3768a0d
fturannn <furkn.turan@hotmail.com>
2023-09-25 22:51:47 +0300

    Final project document is uploaded

--- MayinTarlasi/src/MineSweeper.java
+++ MayinTarlasi/src/MineSweeper.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 public class MineSweeper {
     public static void main(String[] args) {
@@ -22,22 +21,10 @@ public class MineSweeper {
         String [][] placedBoard = new String[row][column];
 
         System.out.println("Location of the Mines");
-        placer(row, column, placedBoard); // Places mines to the Game Board and shows them
-        boardMaker(row, column, gameBoard); // Builds hidden Game Board
-        while (true){
-            System.out.print("Enter row: ");
-            guessedRow = input.nextInt();
+        placer(row, column, placedBoard); // Method that places mines to the Game Board and shows them at first for code check
+        boardMaker(row, column, gameBoard); // Method that builds hidden Game Board
 
-            System.out.print("Enter Column: ");
-            guessedColumn = input.nextInt();
-
-            if(placedBoard[guessedRow - 1][guessedColumn - 1].equals("-")) {
-                writePoint(row, column, guessedRow, guessedColumn, placedBoard, gameBoard);
-            } else {
-                System.out.println("Game Over!");
-                break;
-            }
-        }
+        run(row, column, placedBoard, gameBoard); // Runs the game
     }
     
     static void placer(int row, int column, String [][] gameBoard) {
@@ -81,42 +68,71 @@ public class MineSweeper {
         }
     }
 
-    static void writePoint(int row, int column, int guessedRow, int guessedColumn, String [][] placedBoard, String [][] gameBoard) {
-        int r = guessedRow - 1;
-        int c = guessedColumn - 1;
-        int count = 0;
-            if (r - 1 >= 0 && c - 1 >= 0 && placedBoard[r - 1][c - 1].equals("*")) {
-                count++;
+    static void run(int row, int column, String [][] placedBoard, String [][] gameBoard) {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            int checkGame = 0; // Game checker; if checkGame = 0 game win, else continue
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    if (placedBoard[i][j].equals("-")) {
+                        checkGame++;
+                    }
+                }
             }
-            if (r - 1 >= 0 && placedBoard[r - 1][c].equals("*")) {
-                count++;
+
+            if (checkGame == 0) {
+                System.out.println("You win! Congratulations!");
+                break;
+            } else {
+                System.out.print("Enter row: ");
+                int guessedRow = input.nextInt();
+
+                System.out.print("Enter Column: ");
+                int guessedColumn = input.nextInt();
+
+                if (placedBoard[guessedRow - 1][guessedColumn - 1].equals("-")) {
+                    int r = guessedRow - 1;
+                    int c = guessedColumn - 1;
+                    int count = 0; // Mine counter and control section
+                    if (r - 1 >= 0 && c - 1 >= 0 && placedBoard[r - 1][c - 1].equals("*")) {
+                        count++;
+                    }
+                    if (r - 1 >= 0 && placedBoard[r - 1][c].equals("*")) {
+                        count++;
+                    }
+                    if (r - 1 >= 0 && c + 1 <= column - 1 && placedBoard[r - 1][c + 1].equals("*")) {
+                        count++;
+                    }
+                    if (c - 1 >= 0 && placedBoard[r][c - 1].equals("*")) {
+                        count++;
+                    }
+                    if (c + 1 <= column - 1 && placedBoard[r][c + 1].equals("*")) {
+                        count++;
+                    }
+                    if (c - 1 >= 0 && r + 1 <= row - 1 && placedBoard[r + 1][c - 1].equals("*")) {
+                        count++;
+                    }
+                    if (r + 1 <= row - 1 && placedBoard[r + 1][c].equals("*")) {
+                        count++;
+                    }
+                    if (r + 1 <= row - 1 && c + 1 <= column - 1 && placedBoard[r + 1][c + 1].equals("*")) {
+                        count++;
+                    }
+                    gameBoard[r][c] = String.valueOf(count); //Assign the number of mines surrounding to selected index on Game Board
+                    placedBoard[r][c] = String.valueOf(count); //Assign the number of mines surrounding to selected index on Placed Board
+
+                    for (int i = 0; i < row; i++) { // Print Game Board's final situation
+                        for (int j = 0; j < column; j++) {
+                            System.out.print(gameBoard[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("==================================");
+                } else {
+                    System.out.println("Boooom!!! You stepped on a mine!! Game Over!");
+                    break;
+                }
             }
-            if (r - 1 >= 0 && c + 1 < column - 1 && placedBoard[r - 1][c + 1].equals("*")) {
-                count++;
-            }
-            if (c - 1 >= 0 && placedBoard[r][c - 1].equals("*")) {
-                count++;
-            }
-            if (c + 1 < column - 1 && placedBoard[r][c + 1].equals("*")) {
-                count++;
-            }
-            if (c - 1 >= 0 && r + 1 < row - 1 && placedBoard[r + 1][c - 1].equals("*")) {
-                count++;
-            }
-            if (r + 1 < row - 1 && placedBoard[r + 1][c].equals("*")) {
-                count++;
-            }
-            if (r + 1 < row - 1 && c + 1 < column - 1 && placedBoard[r + 1][c + 1].equals("*")) {
-                count++;
-            }
-        gameBoard[r][c] = String.valueOf(count);
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
-                System.out.print(gameBoard[i][j] + " ");
-            }
-            System.out.println();
         }
-        System.out.println("==================================");
     }
 }
